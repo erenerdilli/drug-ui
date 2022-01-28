15b647c94305641a228566945dbe9bfb95d30981
erenerdilli <erenerdilli@gmail.com>
2022-01-29 01:01:53 +0200

    introduce debounce for input text change

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { Input, Button } from 'antd';
 import useStyles from './styles';
+import debounce from 'lodash.debounce';
 
 const SearchBar = ({ onSearch }) => {
 	const classes = useStyles();
@@ -11,13 +12,22 @@ const SearchBar = ({ onSearch }) => {
 		onSearch({ query })
 	}
 
+	const handleInputOnChange = (event) => {
+		setQuery(event.target.value)
+		onSearch({ query })
+	}
+
+	const debouncedChangeHandler = useMemo(
+		() => debounce(handleInputOnChange, 1000)
+	)
+
 	return (
 		<>
 			<h2>Search</h2>
 			<div className={classes.root}>
 				<Input
 					placeholder="You can search by drug name or by disease..."
-					onChange={event => setQuery(event.target.value)}
+					onChange={debouncedChangeHandler}
 				/>
 				<div className={classes.searchBtn}>
 					<Button onClick={handleSearchClick}>Search</Button>
