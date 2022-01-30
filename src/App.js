dfa86ffb27b88cc054bd3a0a7bbfd068592bf7cc
Ebru Kaya <ebru@ebrukaya.me>
2022-01-30 16:22:55 +0100

    react-query fecthing is added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
+import { Spin } from 'antd';
 import * as DrugApi from 'api/drug';
 import { DrugItem, Header, SearchBar } from './components';
 import useStyles from './globalStyles';
@@ -9,43 +10,26 @@ const App = () => {
   const classes = useStyles();
   const [query, setQuery] = useState(null);
 
-  // const { isLoading, isError, error, data } = useQuery('drugs', () =>
-  //   DrugApi.findByNameOrDisease({ query })
-  // );
-
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(undefined);
-
-  const fetchDrugsByQuery = async ({ query }) => {
-    try {
-      setLoading(true)
-      const data = await DrugApi.findByNameOrDisease({ query })
-      setData(data ? data.data : [])
-    } catch (error) {
-      setError(error.message)
-    } finally {
-      setLoading(false)
-    }
-  };
-
-  useEffect(() => {
-    fetchDrugsByQuery({ query })
-  }, [query]);
+  const { isLoading, error, data } = useQuery(['drugs', query], () =>
+    DrugApi.fetchDrugs({ query })
+  );
 
   const handleSearchChange = (query) => {
     setQuery(query ? query : undefined);
-  }
+  };
 
   return (
     <div className={classes.root}>
       <Header />
       <SearchBar onChange={handleSearchChange} />
-      {loading && <div style={{ padding: 50 }}><h1>Loading...</h1></div>}
+      {isLoading && <div style={{ padding: 50 }}><center><Spin size='large' /></center></div>}
       {error && <div>Error: {error.message}</div>}
-      {data && data.map(drug => <DrugItem key={drug.id} data={drug} />)}
+      {data && (
+        data.length > 0 ? (
+          data.map(drug => <DrugItem key={drug.id} data={drug} />)
+        ) : <div>No records found</div>)}
     </div>
-  )
+  );
 };
 
 export default App;
--- src/api/drug.js
+++ src/api/drug.js
@@ -1,11 +1,8 @@
 import axios from 'utils/axios'
 
-export const getAll = async () => {
-  return await axios.get('/drugs')
-}
-
-export const findByNameOrDisease = async ({ query }) => {
-  return await axios.get('/drugs', {
+export const fetchDrugs = async ({ query }) => {
+  const response = await axios.get('/drugs', {
     params: { query }
-  })
-}
+  });
+  return response.data;
+};
